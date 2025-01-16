45b20a245417a4afaef80c7fe7f1eef7c786f1a8
nakasyou <syoutarou1103@outlook.jp>
2025-01-16 17:54:02 +0900

    fix: test

--- src/adapter/cpu/mod.test.ts
+++ src/adapter/cpu/mod.test.ts
@@ -3,7 +3,7 @@ import { CalculatingNode } from '../../types.ts'
 import { CPUAdapter } from './mod.ts'
 
 Deno.test('calculateGradient', async (t) => {
-  await t.step('f(x, y) = x + y, df/dx = 1, df/dy = 1', () => {
+  await t.step('f(x, y) = x + y, df/dx = 1, df/dy = 1', async () => {
     const adapter = new CPUAdapter()
     
     const x = adapter.createTensorFromArray(2)
@@ -14,10 +14,10 @@ Deno.test('calculateGradient', async (t) => {
       right: { type: 'tensor', id: y, requiresGrad: true },
     }
     const grads = adapter.calculateGradient(f)
-    assertEquals(grads[x].data, 1)
-    assertEquals(grads[y].data, 1)
+    assertEquals(await grads[x].toArray(), 1)
+    assertEquals(await grads[y].toArray(), 1)
   })
-  await t.step('f(x, y) = x + y + y, df/dx = 1, df/dy = 2', () => {
+  await t.step('f(x, y) = x + y + y, df/dx = 1, df/dy = 2', async () => {
     const adapter = new CPUAdapter()
     const x = adapter.createTensorFromArray(2)
     const y = adapter.createTensorFromArray(2)
@@ -31,10 +31,10 @@ Deno.test('calculateGradient', async (t) => {
       },
     }
     const grads = adapter.calculateGradient(f)
-    assertEquals(grads[x].data, 1)
-    assertEquals(grads[y].data, 2)
+    assertEquals(await grads[x].toArray(), 1)
+    assertEquals(await grads[y].toArray(), 2)
   })
-  await t.step('Simple dot product', () => {
+  await t.step('Simple dot product', async () => {
     const adapter = new CPUAdapter()
     const x = adapter.createTensorFromArray([1, 2])
     const b = adapter.createTensorFromArray([3, 4])
@@ -44,7 +44,7 @@ Deno.test('calculateGradient', async (t) => {
       right: { type: 'tensor', id: b, requiresGrad: true },
     }
     const grads = adapter.calculateGradient(y)
-    assertEquals(grads[x].data, [3, 4])
-    assertEquals(grads[b].data, [1, 2])
+    assertEquals(await grads[x].toArray(), [3, 4])
+    assertEquals(await grads[b].toArray(), [1, 2])
   })
 })
